a0b9dee8791bbb0b16a629d468153d254ae4c7f1
anubhav singh <anubhavsingh230@gmail.com>
2023-03-09 00:22:10 +0530

    if else practice

--- jsbasic.js
+++ jsbasic.js
@@ -4,3 +4,21 @@ var y = 30; //global declaration
 
 let z = x + y; //not fixed variable 
 console.info(z)
+
+/*
+it  else condition practice
+*/
+const s1 = 12000;
+const s2 = 11000;
+if (s1 < s2) {
+    console.log("salary of first employee is lesser")
+} else {
+    console.log("s1 salary is greater")
+}
+
+var speciestype = "human";
+if (speciestype == "human") {
+    console.log("specie is human")
+} else {
+    console.log("species is alien")
+}
