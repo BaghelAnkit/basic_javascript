6c7f5feeb2fdcec630debc1cfd5ab48b175ada61
anubhav singh <anubhavsingh230@gmail.com>
2023-03-08 23:39:40 +0530

    variable practice

--- jsbasic.js
+++ jsbasic.js
@@ -1,2 +1,6 @@
 console.info("hello javascript")
-console.info("second ")
+const x = 20; //to make variable constant , value can be change
+var y = 30; //global declaration
+
+let z = x + y; //not fixed variable 
+console.info(z)
